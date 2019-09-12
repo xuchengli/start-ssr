5d22d80d93f263bb9067bb41724e6d62f76855ac
Li Xu Cheng <lixucheng@aliyun.com>
2019-09-12 16:07:12 +0800

    add webpack for client and server endpoint

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Koa = require('koa');
-const renderer = require('vue-server-renderer').createRenderer({
-    template: require('fs').readFileSync('./index.template.html', 'utf-8')
+const { createBundleRenderer } = require('vue-server-renderer');
+const serverBundle = require('./dist/vue-ssr-server-bundle.json');
+const clientManifest = require('./dist/vue-ssr-client-manifest.json');
+const template = require('fs').readFileSync('./index.template.html', 'utf-8');
+
+const renderer = createBundleRenderer(serverBundle, {
+  runInNewContext: false, // 推荐
+  template, // （可选）页面模板
+  clientManifest // （可选）客户端构建 manifest
 });
-const createApp = require('./app');
 const app = new Koa();
 
 app.use(async (ctx, next) => {
@@ -13,11 +19,13 @@ app.use(async (ctx, next) => {
     }
 });
 app.use(async ctx => {
-    const vueApp = createApp({ url: ctx.path });
-    const context = {
-        title: 'Vue SSR测试'
-    };
-    const html = await renderer.renderToString(vueApp, context);
+    // const vueApp = createApp({ url: ctx.path });
+    // const context = {
+    //     title: 'Vue SSR测试'
+    // };
+    // const html = await renderer.renderToString(vueApp, context);
+    const context = { url: ctx.path };
+    const html = await renderer.renderToString(context);
     ctx.body = html;
 });
 app.listen(8080, () => {
